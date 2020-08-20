b9b8f269d0f4be1c89725f5baeb6baf513172cc4
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2020-08-20 16:29:05 -0500

    Adding Deleting Buttons

--- proyecto/src/GUI/VentanaInicio.java
+++ proyecto/src/GUI/VentanaInicio.java
@@ -19,7 +19,8 @@ import javax.swing.JPanel;
  */
 public class VentanaInicio extends JFrame implements ActionListener{
     private final JPanel panelPrincipal = new JPanel();
-    private JButton btnAgregarEmpleado, btnAgregarMaquina, btnIniciarProceso, btnSalir;
+    private JButton btnAgregarEmpleado, btnAgregarMaquina, btnIniciarProceso, btnSalir,
+            btnEliminarEmpleado, btnEliminarMaquina;
     
     public VentanaInicio(){
         super("Administrador de Máquinas");
@@ -27,7 +28,7 @@ public class VentanaInicio extends JFrame implements ActionListener{
     }
     
     public void initComponents(){
-        setBounds(300, 300, 300, 400);
+        setBounds(300, 300, 350, 500);
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -43,16 +44,22 @@ public class VentanaInicio extends JFrame implements ActionListener{
         setBtnAgregarEmpleado(new JButton("Agregar Empleado"));
         setBtnAgregarMaquina(new JButton("Agregar Máquina"));
         setBtnIniciarProceso(new JButton("Iniciar Proceso"));
+        setBtnEliminarEmpleado(new JButton("Eliminar Empleado"));
+        setBtnEliminarMaquina(new JButton("Eliminar Máquina"));
         setBtnSalir(new JButton("Salir"));
         
         getBtnAgregarEmpleado().addActionListener(this);
         getBtnAgregarMaquina().addActionListener(this);
         getBtnIniciarProceso().addActionListener(this);
         getBtnSalir().addActionListener(this);
+        getBtnEliminarEmpleado().addActionListener(this);
+        getBtnEliminarMaquina().addActionListener(this);
         
         getPanelPrincipal().add(getBtnAgregarEmpleado());
         getPanelPrincipal().add(getBtnAgregarMaquina());
         getPanelPrincipal().add(getBtnIniciarProceso());
+        getPanelPrincipal().add(getBtnEliminarEmpleado());
+        getPanelPrincipal().add(getBtnEliminarMaquina());
         getPanelPrincipal().add(getBtnSalir());
     }
 
@@ -76,6 +83,14 @@ public class VentanaInicio extends JFrame implements ActionListener{
         
         if(ev.getSource() == getBtnSalir()){
             System.exit(0);
+        }
+        
+        if(ev.getSource() == getBtnEliminarEmpleado()){
+            int Id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID de empleado a eliminar: "));
+        }
+        
+        if(ev.getSource() == getBtnEliminarMaquina()){
+            int Id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID de la maquina a eliminar: "));
         }
     }
 
@@ -140,6 +155,34 @@ public class VentanaInicio extends JFrame implements ActionListener{
      */
     public void setBtnSalir(JButton btnSalir) {
         this.btnSalir = btnSalir;
+    }
+
+    /**
+     * @return the btnEliminarEmpleado
+     */
+    public JButton getBtnEliminarEmpleado() {
+        return btnEliminarEmpleado;
+    }
+
+    /**
+     * @param btnEliminarEmpleado the btnEliminarEmpleado to set
+     */
+    public void setBtnEliminarEmpleado(JButton btnEliminarEmpleado) {
+        this.btnEliminarEmpleado = btnEliminarEmpleado;
+    }
+
+    /**
+     * @return the btnEliminarMaquina
+     */
+    public JButton getBtnEliminarMaquina() {
+        return btnEliminarMaquina;
+    }
+
+    /**
+     * @param btnEliminarMaquina the btnEliminarMaquina to set
+     */
+    public void setBtnEliminarMaquina(JButton btnEliminarMaquina) {
+        this.btnEliminarMaquina = btnEliminarMaquina;
     }
             
 }
