3710cbd2a1e1e19dfcb831cd6dd8d90308be1f15
DAN <daercimi@gmail.com>
2020-08-26 08:16:03 -0500

    corrección empleado_ordenador

--- proyecto/src/Clases/Empleado_Ordenador.java
+++ proyecto/src/Clases/Empleado_Ordenador.java
@@ -5,4 +5,10 @@ public class Empleado_Ordenador extends Empleado{
     public Empleado_Ordenador(String id, String nombre, String apellido){
         super(id,nombre,apellido);
     }
+    
+    public void RealizaAccion(){
+        String idMaq = this.obtenerMaquina();
+        MaquinaOrdenadora maqAux = new MaquinaOrdenadora(idMaq, "","");
+        maqAux.Operacion();
+    }
 }
